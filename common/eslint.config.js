45a937b809d52b47d94acb52f1b51969825cc557
Pradeep Golla <pradeep.g@darwinbox.io>
2025-09-28 23:17:15 +0530

    Add config for jest and eslint

---
+++ common/eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+    {
+        files: ['**/lib/*.{js,mjs,cjs,ts,mts,cts}','**/tests/*.{js,mjs,cjs,ts,mts,cts}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: {
+            globals: { ...globals.browser, ...globals.jest },
+            parserOptions: {
+                project: ['./tsconfig.json','./tests/tsconfig.spec.json'],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        ignores:["dist/**/*"],
+    },
+
+    tseslint.configs.recommended,
+]);
---
+++ common/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+import { createDefaultEsmPreset } from 'ts-jest';
+
+const presetConfig = createDefaultEsmPreset({
+    //...options
+    tsconfig: "./tests/tsconfig.json"
+});
+
+export default {
+    ...presetConfig,
+    testPathIgnorePatterns: ["./*.test.ts"],
+    workerIdleMemoryLimit:"1024mb"
+} satisfies Config;
