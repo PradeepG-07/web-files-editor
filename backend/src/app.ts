bcde37b0952fee66e98470bfa601ea5df1bf515e
Pradeep Golla <pradeep.g@darwinbox.io>
2025-10-26 00:28:41 +0530

    Add redis client implementation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
+import "dotenv/config";
 import express from 'express';
 import indexRouter from './routes/index.route.js';
-import "dotenv/config";
 
 const app = express();
 
---
+++ backend/src/config/RedisClient.ts
@@ -0,0 +1,28 @@
+import { exit } from "process";
+import { createClient } from "redis";
+import { RedisClientMode } from "../utils/types.js";
+
+type RedisClientType =  ReturnType<typeof createClient>;
+
+export default class RedisClient{
+    private static clients: Map<string,RedisClientType> = new Map<string,RedisClientType>();
+    static async getClient(connString: string, forOperation: RedisClientMode = RedisClientMode.ALL): Promise<RedisClientType>{
+        if(!this.clients.has(forOperation)){
+            await this.initialiseConnection(connString, forOperation);
+        }
+        return this.clients.get(forOperation)!;
+    }
+    static async initialiseConnection(connString: string,forOperation: RedisClientMode = RedisClientMode.ALL): Promise<void>{
+        const client = createClient({
+            url: connString,
+        });
+        client.on('error',(err)=>console.log(err));
+        this.clients.set(forOperation,client);
+        try {
+            await client.connect();
+        } catch (error) {
+            console.log(error);
+            exit();
+        }
+    }
+}
